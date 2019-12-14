09337d9ce31f3bda0ab215eae3c0ced1663dd68c
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-14 17:37:10 +0530

    Updates

---
+++ precache-manifest.305e2e8bfc31208bb694e19cf83d6cf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8071ed8771cda529aa20564110ac152",
+    "url": "/globalnews10/index.html"
+  },
+  {
+    "revision": "170ffae462044bc0c022",
+    "url": "/globalnews10/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9c6f712ef23ab9fb9421",
+    "url": "/globalnews10/static/css/main.a36b7c36.chunk.css"
+  },
+  {
+    "revision": "170ffae462044bc0c022",
+    "url": "/globalnews10/static/js/2.35ed986d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/globalnews10/static/js/2.35ed986d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c6f712ef23ab9fb9421",
+    "url": "/globalnews10/static/js/main.411151f0.chunk.js"
+  },
+  {
+    "revision": "9b1a63107ef4683e8640",
+    "url": "/globalnews10/static/js/runtime-main.5c27c2eb.js"
+  }
+]);
