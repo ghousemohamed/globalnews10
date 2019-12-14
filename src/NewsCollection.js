f4578511772e39edd6b7f823fba7bee603c85a43
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-14 18:32:11 +0530

    testing changes

--- src/NewsCollection.js
+++ src/NewsCollection.js
@@ -8,7 +8,7 @@ const NewsCollection=({newsArray})=>{
         <div className='tc'>
         
             {newsArray.map((news, index)=>{
-                return <div className="card dib pa3 mr2 ma2 bw2 grow shadow-5" style={{width: '18rem'}} key={index}>
+                return <div className="card pa3 mr2 ma2 bw2 grow shadow-5 dib" style={{width: '18rem'}} key={index}>
             {news.urlToImage === '' ? <img src='https://farm5.staticflickr.com/4363/36346283311_74018f6e7d_o.png' className="card-img-top" alt="..."/>:<img src={news.urlToImage} className="card-img-top" alt="..."/>}
             <div className="card-body">
             <h5 className="card-title">{news.title}</h5>
