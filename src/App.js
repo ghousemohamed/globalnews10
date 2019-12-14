cb6370c2ef11479d61bf1c487c9322cbdc512332
ghousemohamed <ghousemohamed7126@gmail.com>
2019-12-14 17:13:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,38 +9,108 @@ class App extends React.Component {
     this.state = {
       response: false,
       input: '',
-      newsArray: []
+      newsArray: [],
+      page: 1
     }
   }
-  getResponse = () => {
+  fetchResponse =() => {
+    fetch('http://localhost:3000/pass', {
+            method: 'post',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({
+              input: this.state.input,
+              page: this.state.page
+            })
+          })
+            .then(response => response.json())
+            .then(data => this.setState({newsArray: data.articles}, ()=> {console.log('Logging Now')}))
+            .catch(err => console.log(err));
+  }  
+  refrshResponse =() => {
+    fetch('http://localhost:3000/pass', {
+            method: 'post',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({
+              input: '',
+              page: this.state.page
+            })
+          })
+            .then(response => response.json())
+            .then(data => this.setState({newsArray: []}, ()=> {this.fetchResponse()}))
+            .catch(err => console.log(err));
+  }
+
+  getResponse = (event) => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
     if(this.state.input.length){
+      console.log('this side up');
       this.setState({response: true})
-    fetch(`https://newsapi.org/v2/everything?q=${this.state.input}&apiKey=7e55080898d84e5abfded9455036e449&pageSize=100`)
-        .then(response => response.json())
-        .then(data => this.setState({newsArray: data.articles}, ()=> {console.log('Logging now')}));
+      this.fetchResponse();
+      event.preventDefault();
+      
     }
     else{
+      console.log('this side down')
       this.setState({response: false})
     }
+}
+
+getNext = () => {
+  if(this.state.input.length){
+    console.log('this side up');
+    this.setState({response: true})
+    this.fetchResponse();
+    
+  }
+  else{
+    console.log('this side down')
+    this.setState({response: false})
+  }
 }
 
 onInputChange =(event) => {
   this.setState({input: event.target.value});
 
 }
+onClick = () => {
+  console.log('Does not refreshh');
+}
+pageClickNext = () => {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+  this.setState({page: this.state.page+1}, ()=> { this.getNext()});
+  
+}
+pageClickPrevious = () => {
+  if (this.state.page === 1){
+
+  }
+  else{
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    this.setState({page: this.state.page-1}, ()=> {this.getNext()});
+  }
+}
   render(){
     return(
       <div>
-        <nav className="navbar fixed-top navbar-expand-lg ">
-    <p className='para'>GLOBAL NEWS 10</p>
+      <div></div>
+        <nav className="navbar fixed-top navbar-expand-lg navbarmain">
+    <p className='para' onClick={this.onClick}>GLOBAL NEWS 10</p>
+    {this.state.response === true ? <form className="center form-inline my-2 my-lg-0">
+    <button type="button" className="btn btn-outline-dark mr2 ma2" onClick={this.pageClickPrevious}>Previous</button>
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.onInputChange}/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.getResponse}>Search</button>
+      <button type="button" className="btn btn-outline-dark mr2 ma2" onClick={this.pageClickNext}>Next</button>
+    </form>: 
     <form className="center form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.onInputChange}/>
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.getResponse}>Search</button>
-    </form>
-  
+    </form>}
+    <button type="button" className="btn btn-outline-success btnsuc">Sign In</button>
 </nav>
 <div className="navdiv"></div>
       {this.state.response ? <NewsCollection input={this.state.input} newsArray={this.state.newsArray}/>:<div></div>}
+    {this.state.response ? <div></div> : <article className='vh-100 dt w-100'><div className='dtc v-mid tc white ph3 ph4-l'><h1 className='f6 f2-m f-subheadline-l fw6 tc'>Search for the latest news sorted by relevance, popularity, source and keywords.</h1></div></article>}
+
       </div>
 
     );
@@ -48,3 +118,4 @@ onInputChange =(event) => {
 }
 
 export default App;
+
--- src/NewsCollection.js
+++ src/NewsCollection.js
@@ -4,18 +4,23 @@ import './NewsCollection.css'
 const NewsCollection=({newsArray})=>{
     
     return (
+    
         <div className='tc'>
+        
             {newsArray.map((news, index)=>{
                 return <div className="card dib pa3 mr2 ma2 bw2 grow shadow-5" style={{width: '18rem'}} key={index}>
-            <img src={news.urlToImage} className="card-img-top" alt="..."/>
+            {news.urlToImage === '' ? <img src='https://farm5.staticflickr.com/4363/36346283311_74018f6e7d_o.png' className="card-img-top" alt="..."/>:<img src={news.urlToImage} className="card-img-top" alt="..."/>}
             <div className="card-body">
             <h5 className="card-title">{news.title}</h5>
             <p className="card-text">{news.source.name}</p>
-            <a href={news.url} className="btn btn-primary b">See Story</a>
+            <a href={news.url} className="btn btn-primary b center"  target="_blank" rel="noopener noreferrer">See Story</a>
         </div>
         </div>
             })}
+            
         </div>
+        
+        
 
     );
 }
